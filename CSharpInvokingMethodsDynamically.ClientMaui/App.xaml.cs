2709f0bf090d0a9b22e63e8cf008a4f42342fa3b
Camilo Jaramillo <80411997+camiloJaramillo5@users.noreply.github.com>
2023-08-21 12:03:51 -0500

    Add initial maui project

---
+++ CSharpInvokingMethodsDynamically.ClientMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpInvokingMethodsDynamically.ClientMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CSharpInvokingMethodsDynamically.ClientMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpInvokingMethodsDynamically.ClientMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
